9640fbfac10ae6ac5c79b97bbba7684cefc8d216
danielkleira <daniel_leira@hotmail.com>
2024-02-06 16:04:57 -0300

    fix-create-ticket-return

--- back/src/requests/requests.service.ts
+++ back/src/requests/requests.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import * as fs from 'fs';
-import fsProm from 'fs/promises';
-import * as FormData from 'form-data';
 import 'dotenv/config';
 
 const email = process.env.email;
@@ -71,15 +68,7 @@ export class RequestsService {
       );
       return response.data;
     } catch (error: unknown) {
-      if (
-        axios.isAxiosError(error) &&
-        error.response &&
-        error.response.status === 422
-      ) {
-        return { status: 'OKs' };
-      } else {
-        throw error;
-      }
+      throw error;
     }
   }
 
@@ -88,16 +77,6 @@ export class RequestsService {
       return;
     }
     try {
-      const filename = 'evidence.png';
-      const imagePath = `./${filename}`;
-      fs.writeFileSync(imagePath, file.buffer);
-
-      const form = new FormData();
-      form.append('file', fs.createReadStream(imagePath), {
-        filename: filename,
-        contentType: 'image/png',
-      });
-
       const zendeskResponse = await axios.post(
         'https://vtex7459.zendesk.com/api/v2/uploads.json?filename=v.png',
         file,
@@ -110,7 +89,6 @@ export class RequestsService {
           },
         },
       );
-      fs.unlinkSync(imagePath);
       return zendeskResponse.data.upload.token;
     } catch (error) {
       throw error;
